ba5bdbdc0878527cb43e7283515b5e5975fbb1b3
yzx <yzx@zhenxing.site>
2017-12-08 17:21:43 +0800

    用户上传头像

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -7,12 +7,20 @@
  */
 
 namespace App\Handlers;
+use Image;
 
 class ImageUploadHandler
 {
     protected $allowed_ext = ['png','jpg','gif','jpeg'];
 
-    public function save($file,$folder,$file_prefix)
+    /**
+     * @param UploadedFile $file 文件对象
+     * @param string $folder 保存路径
+     * @param string $file_prefix 文件前缀
+     * @param bool|int 是否限制大小
+     * @return array|bool
+     */
+    public function save($file,$folder,$file_prefix,$max_width = false)
     {
         //存储路径如：uploads/images/avatars/201712/08/
         $folder_name = "uploads/images/$folder/" . date('Ym',time()) . '/' . date('d',time()) . '/';
@@ -33,12 +41,36 @@ class ImageUploadHandler
 
         $folder_name = rtrim($folder_name,'/');
         $path = rtrim(config('app.url'),'/') . "/$folder_name/$filename";
+
         //移动文件
         $file->move($upload_path,$filename);
+
+        //如果限制了图片宽度，进行裁剪
+        if ($max_width && $extension != 'gif') {
+            $this->reduceSize($upload_path . '/' . $filename,$max_width);
+        }
+
 
         return [
             'path'  =>  $path
         ];
 
+    }
+
+    public function reduceSize($file_path,$max_width)
+    {
+        $image = Image::make($file_path);
+
+        //调整大小
+        $image->resize($max_width,null,function($constraint){
+            //设定宽度是$max_width,高度等比绽放
+            $constraint->aspectRatio();
+
+            //防止截图时图片尺寸变大
+            $constraint->upsize();
+
+        });
+
+        $image->save();
     }
 }
