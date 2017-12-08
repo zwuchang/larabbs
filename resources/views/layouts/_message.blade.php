b7fda617a35bbcae47908466397a455e6e70861f
yzx <yzx@zhenxing.site>
2017-12-08 15:54:03 +0800

    个人资料编辑

---
+++ resources/views/layouts/_message.blade.php
@@ -0,0 +1,19 @@
+@if (Session::has('message'))
+    <div class="alert alert-info">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+        {{ Session::get('message') }}
+    </div>
+@endif
+
+@if (Session::has('success'))
+    <div class="alert alert-success">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+        {{ Session::get('success') }}
+    </div>
+@endif
+
+@if (Session::has('danger'))
+    <div class="alert alert-danger">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+    </div>
+@endif
