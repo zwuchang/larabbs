75e0b5b1f779ba4e112d748f322114b3dd8799ea
yzx <yzx@zhenxing.site>
2017-12-12 12:23:00 +0800

    Generated topics

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,5 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 /* Auth::routes() end */
 
 Route::resource('users','UsersController',['only'=>['show','update','edit']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
